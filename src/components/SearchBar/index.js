e29d9a2283366c98385888f73ae025aa3de21102
Rian Kurniawan <riankurcyberdeepweb@gmail.com>
2022-03-28 23:01:34 +0700

    update state and event handling

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,28 +1,26 @@
 // function Components named Searchbar, has no props
-// const Searchbar = () => {
-//     return (
-//       <form id="form" className="formSearch">
-//         <div className="form-group">
-//           <input type="text" required />
-//           <button id="btnSearch" type="submit">
-//             Search
-//           </button>
-//         </div>
-//       </form>
-//     );
-//   };
-  
-//   export default Searchbar;
-
-  const Search = (props) => {
+const Search = () => {
     return (
-      <form onSubmit={props.handleSubmit} className="formSearch">
+      <form className="formSearch">
         <div className="form-group">
-          <input type="text" name="query" placeholder="Search gif..." required />
-          <input type="submit" id="btnSearch" value="Search" />
+        <input type="text" name="query" placeholder="Search gif..." required />
+        <input type="submit" id="btnSearch" value="Search" />
         </div>
       </form>
     );
   };
   
   export default Search;
+
+  // const Search = (props) => {
+  //   return (
+  //     <form onSubmit={props.handleSubmit} className="formSearch">
+  //       <div className="form-group">
+  //         <input type="text" name="query" placeholder="Search gif..." required />
+  //         <input type="submit" id="btnSearch" value="Search" />
+  //       </div>
+  //     </form>
+  //   );
+  // };
+  
+  // export default Search;
--- src/pages/SearchGif/index.js
+++ src/pages/SearchGif/index.js
@@ -20,18 +20,22 @@ import React from "react"
 const SearchGif = () => {
   const [images, setImages] = React.useState([])
   const [text, setText] = React.useState("")
-  const KEY = "zona4GlIviCYdEjsOyPuej16nsO09jfC"
-
-  const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY
+  const API_KEY = process.env.REACT_APP_API_KEY
 
   const getImages = async query => {
     const LIMIT = 12
-    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${KEY}&q=${text}&limit=${LIMIT}`
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${text}&limit=${LIMIT}`
     const images = await fetch(endpoint).then(res => res.json())
     setImages(images.data)    
   }
 
   const handleSubmit = e => {
+    e.preventDefault()
+    const query = e.target.query.value
+    getImages(query)
+  }
+
+  const handleChange = e => {
     e.preventDefault()
     setText(prevState => (prevState = e.target.value))
     getImages(text)
@@ -46,10 +50,11 @@ const SearchGif = () => {
     // </div>
 
     <div className="container">
-      <h1>Exercise Module 3 Session 1</h1>
-      <form className="formSearch">
+      <h1>Exercise Module 2 Session 1</h1>
+      <form onSubmit={handleSubmit} className="formSearch">
         <div className="form-group">
-          <input onChange={(e) => handleSubmit(e)} type="text" value={text} name="text" placeholder="Search gif..." required />
+          <input onChange={(e) => handleChange(e)} type="text" value={text} name="text" placeholder="Search gif..." required />
+          <input type="submit" id="btnSearch" value="Search" />
         </div>
       </form>
       {images.map(image => (
